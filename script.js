b7ee118f0e20bd11f139538ea429c5769d4fabcf
Munteanu <munteanuvictoria1@gmail.com>
2023-02-03 15:16:30 +0100

    computer play added

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert("Hello there!");
+function computerPlay(){
+    var sides = [
+        "Rock",
+        "Paper",
+        "Scissors"
+    ];
+
+     var index= Math.floor(Math.random() * sides.length)
+     console.log(sides[index]); 
+     
+   
+}
+
+
+computerPlay();
