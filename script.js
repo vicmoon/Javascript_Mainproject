8362f3bd60bb8b0715c450f5ebff68b01e4e8576
Munteanu <munteanuvictoria1@gmail.com>
2023-02-03 17:22:10 +0100

    update

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 function computerPlay(){
-    var sides = [
+    const sides = [
         "Rock",
         "Paper",
         "Scissors"
     ];
 
-     var index= Math.floor(Math.random() * sides.length)
+     const index= Math.floor(Math.random() * sides.length)
      console.log(sides[index]); 
      
    
 }
 
-
 computerPlay();
+
+
+function oneRound(playerSelection, computerSelection){
+    let choice = window.prompt("Enter your choice");
+    playerSelection=choice;
+
+    if (choice == "Rock" && computerSelection ==  "Paper") {
+        return prompt("You lose, Rock beats Paper");    
+    } if (choice == "Paper" && computerSelection == "Scissors"){
+        return prompt("You lose, Scissors beats Paper");
+    } if (choice =="Scissors" && computerSelection  == "Rock"){
+        return prompt("You lose, Rock beats Scissors");
+    }
+    
+    else {
+        return ("Game over!");
+    }
+    
+
+    
+}
+oneRound();
