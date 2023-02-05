3f6f04479f9aa15a04434bb3ad90a5fec0fff571
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2023-02-05 15:05:53 +0100

    update

--- src/script.js
+++ src/script.js
@@ -1,5 +1,11 @@
+const result = document.querySelector("#result");
+const computerSelection = document.querySelector("#computer");
+const playerSelection = document.querySelector("#player");
+const choice = window.prompt("Enter your choice : Paper, Rock or Scissors");
+const computerSelection = computerPlay();
+const playerSelection = playerPlay();
+
 function computerPlay() {
-  const computerSelection = document.querySelector("#computer");
   const choice = ["Rock", "Paper", "Scissors"];
   const index = Math.floor(Math.random() * choice.length);
   computerSelection.innerHTML = choice[index];
@@ -7,21 +13,5 @@ function computerPlay() {
 }
 
 function playerPlay() {
-  const playerSelection = document.querySelector("#player");
   playerSelection.innerHTML = choice;
 }
-
-function playRound(playerSelection, computerSelection) {
-  if (computerSelection != playerSelection) {
-    alert("Play again!");
-  }
-}
-
-const result = document.querySelector("#result");
-const choice = window.prompt("Enter your choice : Paper, Rock or Scissors");
-const computerSelection = computerPlay();
-const playerSelection = playerPlay();
-result.innerHTML = "The round result should be here";
-
-console.log((playerSelection, computerSelection));
-playRound();
