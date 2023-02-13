07bb30fef8334147d31e748524acc70424d61733
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2023-02-13 11:17:34 +0100

    functions update

--- src/script.js
+++ src/script.js
@@ -2,17 +2,18 @@ const computerSelection = document.querySelector("#computer");
 const playerSelection = document.querySelector("#player");
 const possibleChoices = document.querySelectorAll("button");
 const result = document.querySelector("#result");
+let roundNumber = 0;
+
 
 function greeting(){
    alert('Hello, let\'s start playing! Push a button :)');
-}
+} 
 greeting();
 
 
 
-
 function computerPlay() {
-  const compChoice = Math.floor(Math.random() * 3) +1;
+  const compChoice = Math.floor(Math.random() * possibleChoices.length);
   console.log(compChoice);
 
   if (compChoice === 1) {
@@ -28,55 +29,53 @@ function computerPlay() {
 }
 
 
-let playerScore= 0;
-let computerScore =0;
 
-function oneRound(){
-if (playerChoice === computerChoice){
-  finalResult = ' It\'s a draw';
+function playingRound(){
+
+  let playerScore= 0;
+  let computerScore =0;
+
+  if (playerChoice === computerChoice){
+    finalResult = ' It\'s a draw';
   } 
   
- if ((playerChoice === 'Paper' && computerChoice === 'Rock' )
+  if ((playerChoice === 'Paper' && computerChoice === 'Rock' )
   ||(playerChoice === 'Rock' && computerChoice === 'Scissors')
-  || (playerChoice === 'Scissors' && computerChoice === 'Paper')
- ){
-   finalResult = 'You win!';
-   playerScore++;
- }
+  || (playerChoice === 'Scissors' && computerChoice === 'Paper')){
+    finalResult = 'You win!';
+    playerScore++;
+  }
 
- if ((playerChoice === 'Paper' && computerChoice === 'Scissors')
- ||(playerChoice === 'Scissors' && computerChoice === 'Rock')
- ||(playerChoice === 'Rock' && computerChoice === 'Paper')
-
-  ){
+  if ((playerChoice === 'Paper' && computerChoice === 'Scissors')
+  ||(playerChoice === 'Scissors' && computerChoice === 'Rock')
+  ||(playerChoice === 'Rock' && computerChoice === 'Paper')){
     finalResult = 'You lost!'
     computerScore++;
   }
    
-
-
-console.log(`Computer score ${computerScore}`);
-console.log(`Player score ${playerScore}`);
-result.innerHTML= finalResult
+  console.log(`Computer score ${computerScore}`);
+  console.log(`Player score ${playerScore}`);
+  result.innerHTML= finalResult
 
 }
 
- let roundNumber = 0;
+
 
  function roundsCount(){
   roundNumber++;
   alert(`Round  ${roundNumber}`)
   if (roundNumber === 5)
   alert("Time\'s up!")
+ 
 }
 
 possibleChoices.forEach((possibleChoice) =>
 possibleChoice.addEventListener("click", (e) => {
   playerChoice = e.target.id;
-  console.log(playerChoice);
   playerSelection.innerHTML = playerChoice;
+  console.log(playerChoice);
   computerPlay()
-  oneRound()
+  playingRound()
   roundsCount()
 })
 );
